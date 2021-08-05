0349715988a29b044501785cb55f7c2d3762f9ad
Bricx Carasco <bricxraincarasco21@gmail.com>
2021-08-05 21:22:13 +0800

    remove unnecessary files

--- components/PageChange/PageChange.js
+++ components/PageChange/PageChange.js
@@ -1,10 +1,3 @@
-import React from "react";
-
-// reactstrap components
-// import { Spinner } from "reactstrap";
-
-// core components
-
 export default function PageChange(props) {
   return (
     <div>
